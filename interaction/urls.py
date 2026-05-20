8a98f5a03cc4c85a90175fc53e14cf6ef124ffac
Gouransh <gouranshgupta01@gmail.com>
2026-05-20 21:23:16 +0530

    feat: implemented the viewset and the urls for the tag model

---
+++ interaction/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path,include
+
+from .views import TagViewSet
+
+router = DefaultRouter()
+router.register(r'tags',TagViewSet)
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
