58f6481dcec3b069ee7f9c50111acbb3cb009af6
Weaver1209 <bombcyclone12@gmail.com>
2026-05-21 15:41:24 +0530

    feat:add serializers and viewsets

--- studios/serializers.py
+++ studios/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Studio
+from .models import Studio,StudioMembership
+from db.contrib.auth.models import User
 
 class StudioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Studio
-        fields = ['id', 'name', 'description', 'created_at'] 1
+        fields = ['id', 'name', 'description', 'created_at']
+class StudioMembershipSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source = 'user.username')
+    class Meta:
+        model = StudioMembership
+        fields = ['id','username','user','studio','role']
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
