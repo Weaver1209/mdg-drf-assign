95e88730ea4dc153860ece3de945167fd118629b
Gouransh <gouranshgupta01@gmail.com>
2026-05-21 16:20:37 +0530

    feat: created the viewset and handled the urls for the comment model

--- interaction/urls.py
+++ interaction/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path,include
 
-from .views import TagViewSet
+from .views import TagViewSet,CommentViewSet
 
 router = DefaultRouter()
 router.register(r'tags',TagViewSet)
+router.register(r'comments',CommentViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
--- interaction/views.py
+++ interaction/views.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets
-from .models import Tag
-from .serializers import TagSerializer
+from .models import Tag,Comment
+from .serializers import TagSerializer,CommentSerializer
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class TagViewSet(viewsets.ModelViewSet):
-
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-    permission_classes = [IsAuthenticated]
+    queryset = Tag.objects.all() #here we tell which data it can interact with here we are allowing it for whole of the data
+    serializer_class = TagSerializer #here we define which serializer to use for python object and json object interconversion
+class CommentViewSet(viewsets.ModelViewSet):
+     queryset = Comment.objects.all()
+     serializer_class = CommentSerializer
+     
+     #on receiving the request initializing the author of the comment as the user 
+     def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
