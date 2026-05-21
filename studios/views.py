e0f87585411ae4e2d1e250ad08dc8e91f36552c3
Weaver1209 <bombcyclone12@gmail.com>
2026-05-21 15:41:24 +0530

    feat:add serializers and viewsets

--- studios/views.py
+++ studios/views.py
@@ -1,3 +1,16 @@
 from rest_framework import viewsets
 from .models import Studio
-from .serializers import StudioSerializer
+from .serializers import StudioSerializer, UserSerializer, StudioMembershipSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class StudioViewSet(viewsets.ModelViewSet):
+    queryset = Studio.objects.all()
+    serializer_class = StudioSerializer
+
+class StudioMembershipViewSet(viewsets.ModelViewSet):
+    queryset = StudioMembership.objects.all()
+    serializer_class = StudioMembershipSerializer
