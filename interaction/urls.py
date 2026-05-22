f52ce07d6e61e70ad398887143b27c6c3aa31203
Gouransh <gouranshgupta01@gmail.com>
2026-05-22 09:26:43 +0530

    feat: created the viewset and added the endpoint in the urls for the notification model

--- interaction/urls.py
+++ interaction/urls.py
@@ -1,13 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path,include
 
-from .views import TagViewSet,CommentViewSet,AttachmentViewSet
+from .views import TagViewSet,CommentViewSet,AttachmentViewSet,NotificationViewSet
 
 router = DefaultRouter()
 router.register(r'tags',TagViewSet)
 router.register(r'comments',CommentViewSet)
 router.register(r'attachments',AttachmentViewSet)
-
+router.register(r'notifications',NotificationViewSet)
 urlpatterns = [
     path('',include(router.urls)),
 ]
