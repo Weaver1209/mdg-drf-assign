0cd6dbcf5ff6ea8f3c72eea75f538294b6cb9b66
Weaver1209 <bombcyclone12@gmail.com>
2026-05-23 10:43:29 +0530

    feat:setup permissions in views

--- studios/views.py
+++ studios/views.py
@@ -7,10 +7,38 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
 class StudioViewSet(viewsets.ModelViewSet):
-    queryset = Studio.objects.all()
     serializer_class = StudioSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return Studio.objects.filter(members__user=self.request.use
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated(), IsStudioMember()]
+        return [IsAuthenticated(), IsAdminOrLead()]
+    def perform_create(self, serializer):
+        studio = serializer.save()
+        StudioMembership.objects.create(
+            user=self.request.user,
+            studio=studio,
+            role='ADMIN'
 
 class StudioMembershipViewSet(viewsets.ModelViewSet):
     queryset = StudioMembership.objects.all()
     serializer_class = StudioMembershipSerializer
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated(), IsStudioMember()]
+        return [IsAuthenticated(), IsStudioAdminOrLead()]
+    def get_queryset(self):
+        studio_id = self.kwargs.get('studio_id')
+        if studio_id:
+            return StudioMembership.objects.filter(studio_id=studio_id)
+        return StudioMembership.objects.none()
