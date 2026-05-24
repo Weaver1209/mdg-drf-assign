65c7702ab8225717742a55cd336a6d33eb405f5f
Gouransh <gouranshgupta01@gmail.com>
2026-05-24 23:38:50 +0530

    feat: added the read only fields for the serializers

--- interaction/serializers.py
+++ interaction/serializers.py
@@ -4,16 +4,19 @@ class TagSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Tag
                 fields = "__all__" 
+                read_only_fields = ['created_at']
 class CommentSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Comment
                 fields = '__all__'
-                read_only_fields = ['author']
+                read_only_fields = ['author', 'created_at', 'updated_at']
 class AttachmentSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Attachment
-                fields = "__all__"    
+                fields = "__all__"   
+                read_only_fields = ['uploaded_by', 'file_size','created_at'] 
 class NotificationSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Notification
-                fields = "__all__"                         
+                fields = "__all__"     
+                read_only_fields = ['recipient','created_at']                    
