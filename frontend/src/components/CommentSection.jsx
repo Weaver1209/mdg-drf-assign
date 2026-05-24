cc875ba4514a815b8096d7aa5694be8372308ede
Gouransh <gouranshgupta01@gmail.com>
2026-05-24 15:50:58 +0530

    feat: created a comment section component

---
+++ frontend/src/components/CommentSection.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import api from '../api';
+
+export default function CommentSection({ taskId }) {
+  const [comments, setComments] = useState([]);
+  const [content, setContent] = useState('');
+
+  const fetchComments = async () => {
+    const res = await api.get(`/comments/?task_id=${taskId}`);
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, [taskId]);
+
+  const submitComment = async (e) => {
+    e.preventDefault(); // prevent the browser to refresh and set all the values to initials
+
+    await api.post('/comments/', { task_id: taskId,content,});
+
+    setContent('');
+    fetchComments();
+  };
+
+  return (
+    <div>
+      <h3>Comments</h3>
+
+      {comments.map((comment) => (
+        <div key={comment.id}>
+          <p>{comment.content}</p>
+        </div>
+      ))}
+
+      <form onSubmit={submitComment}>
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Write a comment"
+        />
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
+}
